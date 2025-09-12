6ed981a3f8abb78183330fd942409f94523e4606
FilipVondrak <filip.vondrak4@seznam.cz>
2025-09-12 23:10:40 +0200

    Feat: added default styling for markdown with tailwindcss/typography

--- src/app/notes/[...slug]/page.tsx
+++ src/app/notes/[...slug]/page.tsx
@@ -1,6 +1,9 @@
 import { getPostData } from "@/app/lib/notes";
 import { notFound } from "next/navigation";
-
+import Markdown from 'react-markdown'
+import remarkGfm from "remark-gfm";
+//import 'github-markdown-css'
+import "./note-style.css"
 
 export async function generateStaticParams() {
     return [
@@ -23,11 +26,16 @@ export default async function Page({ params }: { params: Promise<{ slug: string[
 
   return (
       <article className={"h-full w-full text-text"}>
+
           <div>Post path: {path}</div>
           <div>Post data: {noteData.title}</div>
           <br />
           {
-              noteData.contentHtml
+              <div className={"prose dark:prose-invert max-w-none prose-blue prose-headings:underline text-text"}>
+                  <Markdown remarkPlugins={[remarkGfm]}>
+                      { noteData.contentHtml}
+                  </Markdown>
+              </div>
           }
       </article>
   )
--- src/app/notes/not-found.tsx
+++ src/app/notes/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">Note not found</h1>
+      <h1 className="text-2xl text-red-500 font-bold">Note not found</h1>
       <p className="mt-2 text-gray-600">
         The note you are looking for doesn’t exist in your vault.
       </p>
